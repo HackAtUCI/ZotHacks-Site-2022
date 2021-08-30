a449631d899801044fbadc903330617440f2eaf0
Nauxie <abhinav.tumu1@gmail.com>
2021-08-30 20:22:37 +0530

    first schedule final draft

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,3 +1,2 @@
-export { default as Nav } from "./nav/Nav";
-export { default as ScheduleCard } from "./schedule-card/ScheduleCard";
-export { default as LeafRain } from "./leaf-rain/LeafRain";
+export { default as Nav } from "./nav/Nav"
+export { default as LeafRain } from "./leaf-rain/LeafRain"
