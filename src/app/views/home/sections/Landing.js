79ede717e2e2534514b997fc909d7c012b175a02
Randy Huynh <randyhuynh5815@gmail.com>
2022-09-21 21:07:28 -0700

    Added moving jeep with moving title text in landing page

--- src/app/views/home/sections/Landing.js
+++ src/app/views/home/sections/Landing.js
@@ -3,6 +3,8 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 
 import "./Landing.scss";
+import jeepBody from "assets/images/jeep_body.png";
+import jeepWheels from "assets/images/jeep_wheels.png"
 
 const APPLICATION_URL =
 	"https://docs.google.com/forms/d/e/1FAIpQLSdPhGwP7KUTcIa6rWsPlyCswlykZwR-dXCJo78Gxahy0Q3sTA/viewform";
@@ -13,6 +15,12 @@ function Landing() {
 			<div className="intro-wrapper">
 				<div className="title-info">
 					<Fade duration={1000} left>
+						<div className="jeep-wrapper">
+							<img src={jeepBody} />
+							<img src={jeepWheels} />
+						</div>
+					</Fade>
+					<Fade duration={1000} right>
 						<div className="hero-wrapper">
 							<h1 className="glow">ZotHacks</h1>
 							<span className="h3">November 20-21, 2021</span>
