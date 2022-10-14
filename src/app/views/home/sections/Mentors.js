446337e850202a1e2bb04a19a54df320728b75c4
Taesung Hwang <taesungh@uci.edu>
2022-10-14 14:02:23 -0700

    Adjust mentor button component
    - Fix semantics for button-like link (use `<a>` instead of `<button>`)
    - Simplify style with Bootstrap `button-variant` mixin
    - Remove old mentor button graphic

--- src/app/views/home/sections/Mentors.js
+++ src/app/views/home/sections/Mentors.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import mentorship from "assets/images/mentorship.png";
-import mentorButton from "assets/images/mentorButton.png";
 
 import "./Mentors.scss";
 
-const MENTOR_APPLICATION_URL =
-	"https://airtable.com/shr5B94rQ66KWH3Iw";
+const MENTOR_APPLICATION_URL = "https://airtable.com/shr5B94rQ66KWH3Iw";
 
 function Mentors() {
 	return (
@@ -24,10 +21,11 @@ function Mentors() {
 						href={MENTOR_APPLICATION_URL}
 						target="_blank"
 						rel="noreferrer"
+						className="btn mentor-apply-button"
 						data-place="bottom"
-                        data-tip="Make sure to use your UCI email!"
+						data-tip="Make sure to use your UCI email!"
 					>
-						<button className="mentor-apply-button">APPLY TO BE A MENTOR</button>
+						Apply to be a Mentor
 					</a>
 				</div>
 			</div>
