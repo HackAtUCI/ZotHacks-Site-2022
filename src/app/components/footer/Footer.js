8eb4dcb26e9e8fcabc753a0532da887780c81a3a
Taesung Hwang <taesungh@uci.edu>
2022-10-14 21:27:20 -0700

    Fix eslint errors when building project
    - Comment unused route elements in App
    - Rebuild Footer socials to add noreferrer to links
      - Links were using `target="_blank"` without `rel="noreferrer"`
    - Add alt text to Home mentor image

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './Footer.scss';
+import "./Footer.scss";
 
 import MailIcon from "assets/icons/mail_icon.svg";
 import FacebookIcon from "assets/icons/facebook_icon.svg";
@@ -7,19 +7,47 @@ import InstagramIcon from "assets/icons/instagram_icon.svg";
 import LinkedInIcon from "assets/icons/linkedin_icon.svg";
 import YouTubeIcon from "assets/icons/youtube_icon.svg";
 
+const SOCIALS = [
+	{
+		link: "mailto:hack@uci.edu",
+		icon: MailIcon,
+		alt: "Mail",
+	},
+	{
+		link: "https://www.facebook.com/UCI.Hack/",
+		icon: FacebookIcon,
+		alt: "Facebook",
+	},
+	{
+		link: "https://www.instagram.com/hackatuci/",
+		icon: InstagramIcon,
+		alt: "Instagram",
+	},
+	{
+		link: "https://www.linkedin.com/company/hackuci",
+		icon: LinkedInIcon,
+		alt: "LinkedIn",
+	},
+	{
+		link: "https://www.youtube.com/channel/UCeQbk4CMo3mxPHMtY80PtFQ",
+		icon: YouTubeIcon,
+		alt: "YouTube",
+	},
+];
+
 function Footer() {
-    return (
-        <div className="footer">
-            <p>Made with 💖 in Irvine, CA - Hack at UCI</p>
-            <div className="footer-socials">
-                <a href="mailto:hack@uci.edu" target="_blank"><img src={MailIcon} alt="Mail"/></a>
-                <a href="https://www.facebook.com/UCI.Hack/" target="_blank"><img src={FacebookIcon} alt="Facebook"/></a>
-                <a href="https://www.instagram.com/hackatuci/" target="_blank"><img src={InstagramIcon} alt="Instagram"/></a>
-                <a href="https://www.linkedin.com/company/hackuci" target="_blank"><img src={LinkedInIcon} alt="LinkedIn"/></a>
-                <a href="https://www.youtube.com/channel/UCeQbk4CMo3mxPHMtY80PtFQ" target="_blank"><img src={YouTubeIcon} alt="YouTube"/></a>
-            </div>
-        </div>
-    )
+	return (
+		<div className="footer">
+			<p>Made with 💖 in Irvine, CA - Hack at UCI</p>
+			<div className="footer-socials">
+				{SOCIALS.map(({ link, icon, alt }) => (
+					<a key={link} href={link} target="_blank" rel="noreferrer">
+						<img src={icon} alt={alt} />
+					</a>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
