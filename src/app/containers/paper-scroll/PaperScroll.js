9a828679a575d0ed2eec0fa09a0d8b2547fd68a5
Taesung Hwang <taesungh@uci.edu>
2022-10-15 18:06:46 -0700

    Implement paper scroll unrolling animation
    - Replace Fade entrance with unrolling effect which animates scroll edge
      positions and masks the card body to within the scroll edges
    - Scroll unrolls from center when horizontal and from top when vertical
    - Specify initial state in styles and final state in animation keyframes

--- src/app/containers/paper-scroll/PaperScroll.js
+++ src/app/containers/paper-scroll/PaperScroll.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Reveal from "react-reveal/Reveal";
 
 import "./PaperScroll.scss";
 
 function PaperScroll({ children }) {
 	return (
-		<div className="paper-scroll-card">
-			<div className="card-body">{children}</div>
-		</div>
+		<Reveal effect="unroll-scroll" duration={2000} delay={100}>
+			<div className="paper-scroll-card">
+				<div className="scroll-edge-start" />
+				<div className="card-body">{children}</div>
+				<div className="scroll-edge-end" />
+			</div>
+		</Reveal>
 	);
 }
 
